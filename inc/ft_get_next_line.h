757a76969c8acb4c2ded3a89673b5b93f698a012
cghael <55437891+cghael@users.noreply.github.com>
2020-04-05 17:02:48 +0300

    add gnl

---
+++ inc/ft_get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_get_next_line.h                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cghael <cghael@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/26 18:07:54 by cghael            #+#    #+#             */
+/*   Updated: 2019/10/09 16:26:39 by cghael           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define BUFF_SIZE 1
+
+int		ft_get_next_line(const int fd, char **line);
+
+#endif
